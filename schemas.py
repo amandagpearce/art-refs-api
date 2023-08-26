31bd1ee47d3be4384a33590cdddb8ffac6a814f7
Amanda Pearce <amandagpearce@gmail.com>
2023-08-26 19:50:03 -0300

    schemas update

--- schemas.py
+++ schemas.py
@@ -1,12 +1,17 @@
 from graphene import ObjectType, String, Int, List, Schema
 from graphene_sqlalchemy import SQLAlchemyObjectType
-from models import Series as SeriesModel, Scene as SceneModel
+from models import (
+    Series as SeriesModel,
+    Scene as SceneModel,
+    Movies as MoviesModel,
+    Artwork as ArtworksModel,
+)
 
 
 class SceneType(ObjectType):
     id = Int()
-    seriesId = Int()
-    artworkId = Int()
+    series_id = Int()
+    artwork_id = Int()
     sceneDescription = String()
 
 
@@ -14,14 +19,40 @@ class Series(ObjectType):
     id = Int()
     title = String()
     year = String()
-    imageUrl = String()
+    image_url = String()
+
+
+class Movies(ObjectType):
+    id = Int()
+    title = String()
+    year = String()
+    image_url = String()
+
+
+class ArtworkType(ObjectType):
+    id = Int()
+    title = String()
+    year = Int()
+    artist = String()
+    size = String()
+    description = String()
+    current_location = String()
+    image_url = String()
+    scenes = List("SceneType")  # Assuming SceneType is defined similarly
 
 
 class Query(ObjectType):
     series = List(Series)
+    movies = List(Movies)
 
     def resolve_series(self, info):
         return SeriesModel.query.all()
+
+    def resolve_movies(self, info):
+        return MoviesModel.query.all()
+
+    def resolve_artworks(self, info):
+        return ArtworksModel.query.all()
 
 
 schema = Schema(query=Query)
