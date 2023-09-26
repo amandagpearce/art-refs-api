74d35604ef665b4fd0cb53a05177507889abbbe7
Amanda Pearce <amandagpearce@gmail.com>
2023-09-26 18:24:09 -0300

    update trakt search

--- shared/trakt_api.py
+++ shared/trakt_api.py
@@ -48,58 +48,48 @@ def fetch_and_populate(production_type, production_data):
 
             if response.status_code == 200:
                 trakt_response = response.json()
-                print("trakt_response", trakt_response)
+                # print("trakt_response", trakt_response)
 
                 if trakt_response:
+                    # If there's only one result, select it
+                    if len(trakt_response) == 1:
+                        selected_result = trakt_response[0]
                     # Check if there are multiple results
-                    if len(trakt_response) > 1:
-                        # Iterate through the results and find the one with a matching production_year
+                    elif len(trakt_response) > 1:
+                        # Iterate to find a matching production_year
                         for result in trakt_response:
                             if result[api_endpoint]["year"] == production_year:
                                 selected_result = result
                                 break  # Stop iterating once a match is found
 
-                        # If no match was found, you can choose a different handling logic or raise an exception
-                        if selected_result is None:
-                            print(
-                                "No matching result found for the specified production_year."
-                            )
-
-                    # If there's only one result, select it
-                    elif len(trakt_response) == 1:
-                        selected_result = trakt_response[0]
-
-                    print("selected_result", selected_result)
-
-                    trakt_id = selected_result[api_endpoint]["ids"]["trakt"]
-
-                    poster_url = fetch_poster(
-                        selected_result[api_endpoint]["ids"]["tmdb"],
-                        tmdb_base_url,
-                    )
-
-                    print("trakt_id", trakt_id)
-                    print("poster_url", poster_url)
-
-                    if trakt_id and poster_url:
-                        production_model = model_class(
-                            id=trakt_id,
-                            productionTitle=production_title,
-                            year=production_year,
-                            imageUrl=poster_url,
+                    if selected_result is None:
+                        print(
+                            "No matching result found for the specified production_year."
                         )
-                        db.session.add(production_model)
-                        db.session.commit()
-
-                        print("trakt_id", trakt_id)
-                        print("len(production_data)", len(production_data))
+                        return
+                    else:
+                        # Get data from production
+                        trakt_id = selected_result[api_endpoint]["ids"]["trakt"]
+                        poster_url = fetch_poster(
+                            selected_result[api_endpoint]["ids"]["tmdb"],
+                            tmdb_base_url,
+                        )
+                        if trakt_id and poster_url:
+                            production_model = model_class(
+                                id=trakt_id,
+                                productionTitle=production_title,
+                                year=production_year,
+                                imageUrl=poster_url,
+                            )
+                            db.session.add(production_model)
+                            db.session.commit()
 
                         if len(production_data) == 1:
-                            print("return trakt_id", trakt_id)
+                            # print("return trakt_id", trakt_id)
                             return trakt_id
-                    else:
-                        print("Trakt query did not return data")
-                        return
+                else:
+                    print("Trakt query did not return data")
+                    return
         else:
             print("production_year and production_title must be provided.")
             return
